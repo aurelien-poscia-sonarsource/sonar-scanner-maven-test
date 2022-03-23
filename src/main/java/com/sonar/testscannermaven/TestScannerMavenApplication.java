481be52b9961df4aa998f2e5e38fa63d900622b5
Aurelien Poscia <aurelien.poscia@sonarsource.com>
2022-03-23 10:58:33 +0100

    initial commit

---
+++ src/main/java/com/sonar/testscannermaven/TestScannerMavenApplication.java
@@ -0,0 +1,13 @@
+package com.sonar.testscannermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestScannerMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestScannerMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonar/testscannermaven/TestScannerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonar.testscannermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestScannerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
