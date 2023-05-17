a22d22a6d09b634d46c3c1b4a62ca6ddc46ece6c
Aurelien Poscia <aurelien.poscia@sonarsource.com>
2023-05-17 11:23:08 +0200

    new file

---
+++ src/main/java/com/sonar/testscannermaven/TestScannerMavenApplication2.java
@@ -0,0 +1,16 @@
+package com.sonar.testscannermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestScannerMavenApplication2 {
+	public static final String lowercase_stupid_extra_long_name_43242874234982789589234568934rt69ew4gheskdfghdsfiuvbas4535734534534oh5h3o4g5hoi35hoi34h5o34ho543hoi53ho45ho34ho532oh45ho534ho53ho5ho345oh543oh5ho34o5hho345 = "TEST";
+	public static final String lowercase_stupid_extra_long_name_243242874234982789589234568934rt69ew4gheskdfghdsfiuvbas4535734534534oh5h3o4g5hoi35hoi34h5o34ho543hoi53ho45ho34ho532oh45ho534ho53ho5ho345oh543oh5ho34o5hho345 = "TEST";
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestScannerMavenApplication2.class, args);
+		System.out.println("Tagada tsoin tsoin");
+	}
+
+}
